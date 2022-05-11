a5fe14836cc326ea50309197895aa97081ea2574
xVambOx <105160137+xVambo@users.noreply.github.com>
2022-05-11 10:50:29 +0530

    Update Party.ts

--- src/commands/Pokemon/Party.ts
+++ src/commands/Pokemon/Party.ts
@@ -50,7 +50,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `${text}`,
-      footerText: "🎇 Beyond 🎇",
+      footerText: "©Celestial 2022",
       buttons: buttons,
       headerType: 1,
     };

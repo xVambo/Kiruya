4192ef6fcd74f9f3e8c1d0b4fba0da591863fb03
synbee <97864273+synbee@users.noreply.github.com>
2022-05-11 10:56:33 +0530

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -20,14 +20,14 @@ export default class Command extends BaseCommand {
     const filteredMap = this.client.config.mods
       .map((mod) => this.client.getContact(mod))
       .filter((user) => user);
-    let text = "🎆 Celestial Mods🎆\n\n";
+    let text = " Celestial Mods\n\n";
     filteredMap.forEach(
       (user, index) =>
         (text += `#${index + 1}\n🎉 *Username: ${
           user.notify || user.vname || user.name || "null"
-        }*\n🎆 *Contact: https://wa.me/+${user?.jid?.split("@")[0]}*\n\n`)
+        }*\n *Contact: https://wa.me/+${user?.jid?.split("@")[0]}*\n\n`)
     );
-    text += `\n 🎆©Celestial 2022🎆`;
+    text += `\n ©Celestial 2022`;
     return void M.reply(text);
   };
 }

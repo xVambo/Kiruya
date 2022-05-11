08bcdddbb40b945b90b4edc68dcbcf70042d4104
synbee <97864273+synbee@users.noreply.github.com>
2022-05-11 11:23:31 +0530

    Update EventHandler.ts

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -53,7 +53,7 @@ export default class EventHandler {
       }
       const buttonMessage: any = {
         contentText: `${text}`,
-        footerText: "©Celestial 2022",
+        footerText: "© Celestial 2022",
         buttons: buttons,
         headerType: 1,
       };

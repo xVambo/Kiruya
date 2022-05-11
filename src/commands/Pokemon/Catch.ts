b4859235b57713e5aee61e683faf9194380049fe
xVambOx <105160137+xVambo@users.noreply.github.com>
2022-05-11 23:56:50 +0530

    Update Catch.ts

--- src/commands/Pokemon/Catch.ts
+++ src/commands/Pokemon/Catch.ts
@@ -80,7 +80,7 @@ export default class Command extends BaseCommand {
       );
       if (data.party.length < 6) {
         const buttonMessage: any = {
-          contentText: `Well done. You caught a level ${Level} ${this.client.util.capitalize(
+          contentText: `🎉Well done. You caught a level ${Level} ${this.client.util.capitalize(
             Name
           )}.`,
           footerText: "© Celestial 2022",

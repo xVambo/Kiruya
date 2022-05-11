5e225439dec567fa68b23d7584ad549bbc1fcce1
synbee <97864273+synbee@users.noreply.github.com>
2022-05-11 10:34:47 +0530

    Update Withdraw.ts

--- src/commands/Economy/Withdraw.ts
+++ src/commands/Economy/Withdraw.ts
@@ -48,7 +48,7 @@ export default class Command extends BaseCommand {
 
     const buttonMessage: any = {
       contentText: `You have withdrawn *${amount} gold* from your bank.`,
-      footerText: "🎇 Beyond 🎇",
+      footerText: "© Celestial 2022",
       buttons: buttons,
       headerType: 1,
     };

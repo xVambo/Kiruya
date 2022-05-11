411c65188236e33726729270815874ba8871e640
xVambOx <105160137+xVambo@users.noreply.github.com>
2022-05-11 10:40:20 +0530

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -20,14 +20,14 @@ export default class Command extends BaseCommand {
     const filteredMap = this.client.config.mods
       .map((mod) => this.client.getContact(mod))
       .filter((user) => user);
-    let text = "🎆Beyond Owners🎆\n\n";
+    let text = "🎆 Celestial Mods🎆\n\n";
     filteredMap.forEach(
       (user, index) =>
         (text += `#${index + 1}\n🎉 *Username: ${
           user.notify || user.vname || user.name || "null"
         }*\n🎆 *Contact: https://wa.me/+${user?.jid?.split("@")[0]}*\n\n`)
     );
-    text += `\n 🎆BEYOND🎆`;
+    text += `\n 🎆©Celestial 2022🎆`;
     return void M.reply(text);
   };
 }

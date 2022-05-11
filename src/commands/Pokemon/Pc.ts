a04a4d342d4fd4ee2bd2be5a53529f83fd63d9ea
xVambOx <105160137+xVambo@users.noreply.github.com>
2022-05-11 10:51:04 +0530

    Update Pc.ts

--- src/commands/Pokemon/Pc.ts
+++ src/commands/Pokemon/Pc.ts
@@ -46,7 +46,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `${text}`,
-      footerText: "🎇 Beyond 🎇",
+      footerText: "©Celestial 2022",
       buttons: buttons,
       headerType: 1,
     };

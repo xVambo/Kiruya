28cbfe73819960f8f6960cd8c8079e5deb887f68
xVambOx <105160137+xVambo@users.noreply.github.com>
2022-05-11 12:30:41 +0530

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -15,8 +15,6 @@ export default class Command extends BaseCommand {
   }
 
   run = async (M: ISimplifiedMessage): Promise<void> => {
-    if (!this.client.config.mods || !this.client.config.mods[0])
-      return void M.reply("*[UNMODERATED]*");
     const filteredMap = this.client.config.mods
       .map((mod) => this.client.getContact(mod))
       .filter((user) => user);

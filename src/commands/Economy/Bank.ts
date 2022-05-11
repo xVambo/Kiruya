00c30c29f98d61eea57d0605634c2043e45255bd
synbee <97864273+synbee@users.noreply.github.com>
2022-05-11 10:29:38 +0530

    Update Bank.ts

--- src/commands/Economy/Bank.ts
+++ src/commands/Economy/Bank.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
 
     const buttonMessage: any = {
       contentText: `🏦 *Bank | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`,
-      footerText: "🎇 Beyond 🎇",
+      footerText: "© Celestial 2022",
       buttons: buttons,
       headerType: 1,
     };
